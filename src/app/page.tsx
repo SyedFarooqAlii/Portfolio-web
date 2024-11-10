c378cb91dd1e89059008a29b8ca843d9a0b96bbc
SyedFarooqAlii <160501530+SyedFarooqAlii@users.noreply.github.com>
2024-11-10 23:37:43 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+import Main from './Main/page'
+import About from './About/page'
+import Skill from './Skill/page'
+import Project from './Project/page'
+import Contact from './Contact/page'
+const Home = () => {
+  return (
+    <div>
+      <Main/>
+<About/>
+<Skill/>
+<Project/>
+<Contact/>
+
+
+    </div>
+
+
+
+  )
+}
+
+export default Home
